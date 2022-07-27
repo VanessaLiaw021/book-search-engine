73b62b93de3b5d6f49cc38d459e7dc16a6ee6699
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-27 19:52:22 -0400

    added content inside queries.js for the query user

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,17 @@
 //Import required packages
 import { gql } from '@apollo/client';
+
+//Export QUERY_ME
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username 
+            email 
+            bookCount
+            savedBooks {
+            
+            }
+        }
+    }
+`;
