a432cf73ebcb31ba3c31bb409fca66f06eb91ba1
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-27 18:34:06 -0400

    refactor server.js to use ApolloServer and schema

--- server/server.js
+++ server/server.js
@@ -1,21 +1,29 @@
+//Import required packages and files
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require("./schemas");
 
+//Start the express server 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Implement ApolloServer
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers, 
+  context: authMiddleware
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("*", (req, res) => res.sendFile(path.join(_dirname, "../client/build/index.html")));
+
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+if (process.env.NODE_ENV === 'production') app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+//Listen to the port
+db.once('open', () => app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`)));
