e20ec15776866212e9b1ce0cfdf4f5eda4d911bc
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-28 10:52:58 -0400

    figure out the save book to saved book pages

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -71,7 +71,7 @@ const SearchBooks = () => {
     }
 
     try {
-      await saveBook({ variables: { book: { ...bookToSave }}});
+      await saveBook({ variables: { bookData: { ...bookToSave }}});
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,7 +4,7 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 
     type User {
-        _id: ID
+        _id: ID!
         username: String 
         email: String 
         bookCount: Int
@@ -14,19 +14,19 @@ const typeDefs = gql`
     type Book {
         bookId: String!
         authors: [String]
-        title: String! 
+        title: String
         description: String 
         image: String 
         link: String
     }
 
     input BookInput {
-        bookId: String!
+        bookId: String
         authors: [String]
-        description: String! 
+        description: String
         title: String 
         image: String 
-        link: String!
+        link: String
     }
 
     type Auth {
@@ -41,7 +41,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(input: BookInput!): User
+        saveBook(bookData: BookInput!): User
         removeBook(bookId: String!): User
     }
 `;
